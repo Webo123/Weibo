c0fe57afff66b2ba134b51406154dafbcca9ad32
shitou <shitou@shitou.cn>
2016-01-29 18:04:38 +0800

    初始化项目组

---
+++ WeiboGroup/WeiboGroup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiboGroup
+//
+//  Created by 石超 on 16/1/29.
+//  Copyright © 2016年 石超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiboGroup/WeiboGroup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiboGroup
+//
+//  Created by 石超 on 16/1/29.
+//  Copyright © 2016年 石超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
